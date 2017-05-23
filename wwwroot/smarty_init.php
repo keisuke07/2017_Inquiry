278c7f388dfb79357b6a0feebf4b01edfa8562f1
Miura <you@example.com>
2017-05-23 18:42:50 +0900

    smarty_init.php edit

--- wwwroot/smarty_init.php
+++ wwwroot/smarty_init.php
@@ -1,6 +1,8 @@
 <?php
-// smarty-test.php
-
+// smarty_init.php
+/*
+ * Smarty用初期処理
+ */
 // Smartyのinclude
 require_once( __DIR__ . '/vendor/smarty-3.1.30/libs/Smarty.class.php');
 
@@ -11,14 +13,3 @@ $smarty_obj->setTemplateDir( __DIR__ . '/../smarty/templates/');
 $smarty_obj->setCompileDir( __DIR__ . '/../smarty/templates_c/');
 // エスケープを自動でonにする
 $smarty_obj->escape_html = true;
-
-// Smartyへのデータの入力
-$s = 'データ入力テスト';
-$smarty_obj->assign('val', $s);
-//
-$awk['a'] = '配列のa';
-$awk['b'] = '配列のb';
-$smarty_obj->assign('ar', $awk);
-
-// テンプレートを指定して出力
-$smarty_obj->display('smarty_test.tpl');
